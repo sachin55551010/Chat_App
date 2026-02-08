0cbcf151f85d96e9e7ad6544cab7af5cf21d0bef
Sachin Khatri <sachinbansal3333@gmail.com>
2026-02-08 12:29:13 +0530

    added frontend link

--- backend/src/app.js
+++ backend/src/app.js
@@ -17,7 +17,7 @@ app.use(express.urlencoded({ extended: true, limit: "10mb" }));
 
 app.use(
   cors({
-    origin: [process.env.FRONTEND_URL],
+    origin: [process.env.FRONTEND_URL, process.env.FRONTEND_PRODUCTION_URL],
     credentials: true,
   }),
 );
--- backend/src/utils/socket.js
+++ backend/src/utils/socket.js
@@ -7,7 +7,7 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
   cors: {
-    origin: process.env.FRONTEND_URL, // aaded backend url here
+    origin: [process.env.FRONTEND_URL, process.env.FRONTEND_PRODUCTION_URL], // aaded backend url here
     methods: ["GET", "POST"],
     credentials: true,
   },
